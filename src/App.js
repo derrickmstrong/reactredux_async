da2653d2d5c898879a494d9f2143134cfcf2c44b
Derrick Strong <derrickmstrong@gmail.com>
2021-09-29 14:48:46 -0500

    Render Posts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
+import { ActionCreators } from './store'
+import { bindActionCreators } from 'redux'
 
 const App = () => {
+    const posts = useSelector(state => state.posts)
+    const dispatch = useDispatch()
+    const { fetchPosts } = bindActionCreators(ActionCreators)
+    useEffect(() => {
+        dispatch(ActionCreators.fetchPosts())
+        return () => {}
+    }, [])
+
+    const renderPosts = posts.map(el => <p>{el.title}</p>)
     return (
         <div>
-            Hello from App
+            {renderPosts}
         </div>
     )
 }
